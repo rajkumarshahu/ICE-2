7f1c3e2fc568b676ab44e58f71acf77dcbcb6c0b
Raj Kumar Shahu <rajkumarshahu@gmail.com>
2021-02-05 12:41:56 -0500

    Complete in class exercise 2

--- ICE-2/GameScene.swift
+++ ICE-2/GameScene.swift
@@ -10,62 +10,41 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var entities = [GKEntity]()
+    var graphs = [String: GKGraph]()
     
-    override func didMove(to view: SKView) {
+    private var lastUpdateTime : TimeInterval = 0
+    private var label : SKLabelNode?
+    private var sprite: SKSpriteNode?
+    private var spriteSpeed:CGFloat = 5.0
+    
+    override func sceneDidLoad() {
+        self.lastUpdateTime = 0
         
         // Get label node from scene and store it for use later
         self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        self.sprite = SKSpriteNode(color: UIColor.red, size:CGSize(width: 100.0, height: 100.0))
+        self.addChild(self.sprite!)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        reset()
     }
+
     
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+       
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+        
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
+     
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
@@ -81,8 +60,46 @@ class GameScene: SKScene {
         for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
+    func reset() {
+        self.sprite?.position.y = -700
+    }
+    
+    func checkBounds() {
+        // check if sprite leaves the top boundary
+        if (self.sprite?.position.y)! > 700 {
+            reset()
+        }
+    }
+    
+    func moveSprite() {
+        self.sprite?.position.y += self.spriteSpeed
+        self.sprite?.zRotation -= self.spriteSpeed * 0.025
+        
+        self.sprite?.zPosition = 1
+        self.sprite?.color = UIColor.init(red: 0.0, green: 1.0, blue: 0.0, alpha: 0.5)
+        self.sprite?.setScale(1.5)
+    }
+    
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        
+        // Initialize _lastUpdatetime if it has not already been
+        if(self.lastUpdateTime == 0) {
+            self.lastUpdateTime = currentTime
+        }
+        
+        // Calculate time since last update
+        let dt = currentTime - self.lastUpdateTime
+        
+        // Update entities
+        for entity in self.entities {
+            entity.update(deltaTime: dt)
+        }
+        
+        moveSprite()
+        checkBounds()
+        
+        self.lastUpdateTime = currentTime
     }
 }
